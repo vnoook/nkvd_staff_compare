049cc22acdc3bf0b4d14f4f36ce797ab27fae84f
noook <noook@yandex.ru>
2026-06-09 16:45:14 +0700

    #8 select data from both files

--- nkvd_staff_compare.py
+++ nkvd_staff_compare.py
@@ -6,15 +6,13 @@ import openpyxl
 def str_contains_digit(sequence):
     return any(character.isdigit() for character in sequence)
 
-
 # файлы данных
 file_staff_1c = 'staff_1c.xlsx'
 file_staff_aduser = 'staff_aduser.xlsx'
 
-# подключаюсь к файлам, собираю инфу с нужных колонок в разные списки, сравниваю фио и в файле 1с отмечаю цветом
+# подключаюсь к файлу
 wb_1c = openpyxl.load_workbook(file_staff_1c)
 wb_1c_s = wb_1c.active
-# print(wb_1c, wb_1c_s)
 
 # ищу минимальные и максимальные места, где есть данные
 wb_1c_s_row_begin = 1  # wb_1c_s.min_row
@@ -31,12 +29,33 @@ for fio_list in wb_1c_s.iter_cols(min_col=wb_1c_s_col_begin, max_col=wb_1c_s_col
 list_fio_1c = []
 for fio in fio_list:
     if not str_contains_digit(str(fio)):
-        # print(fio)
-        # print(''.join(fio.strip().lower().split()))
-        list_fio_1c.append(''.join(fio.strip().lower().split()))
+        list_fio_1c.append(''.join(str(fio).strip().lower().split()))
 
-print(list_fio_1c)
+len_list_fio_1c = len(list_fio_1c)
+set_fio_1c = set(list_fio_1c)
+len_set_fio_1c = len(set_fio_1c)
 
+# подключаюсь к файлу
 wb_aduser = openpyxl.load_workbook(file_staff_aduser)
 wb_aduser_s = wb_aduser.active
-# print(wb_aduser, wb_aduser_s)
+
+# ищу минимальные и максимальные места, где есть данные
+wb_aduser_s_row_begin = 1  # wb_aduser_s.min_row
+wb_aduser_s_row_end = wb_1c_s.max_row
+wb_aduser_s_col_begin = 1  # wb_aduser_s.min_column
+wb_aduser_s_col_end = wb_1c_s.max_column
+
+# читаю данные из файла
+for fio_list in wb_1c_s.iter_cols(min_col=wb_aduser_s_col_begin, max_col=wb_aduser_s_col_end,
+                                  min_row=wb_aduser_s_row_begin, max_row=wb_aduser_s_row_end,
+                                  values_only=True):
+    pass
+
+list_fio_aduser = []
+for fio in fio_list:
+    if not str_contains_digit(str(fio)):
+        list_fio_aduser.append(''.join(str(fio).strip().lower().split()))
+
+len_list_fio_aduser = len(list_fio_aduser)
+set_fio_aduser = set(list_fio_aduser)
+len_set_fio_aduser = len(set_fio_aduser)
