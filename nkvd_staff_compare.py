d7b775986b8e2cfd6ed04a5e0315132296d8ddb1
noook <noook@yandex.ru>
2026-06-09 11:26:40 +0700

    #3 read fileы

--- nkvd_staff_compare.py
+++ nkvd_staff_compare.py
@@ -1,21 +1,27 @@
-# надо сравнить два файла xls по колонкам фио и подразделение
-# за главную информацию взять информацию из 1С
+# надо сравнить два файла xls файла по колонкам фио
+# за главную информацию взять информацию из файла 1С
 
 import openpyxl
 
+# файлы данных
 file_staff_1c = 'staff_1c.xlsx'
 file_staff_aduser = 'staff_aduser.xlsx'
 
 # подключаюсь к файлам, собираю инфу с нужных колонок в разные списки, сравниваю фио и в файле 1с отмечаю цветом
-wb_1c = openpyxl.Workbook()
+wb_1c = openpyxl.load_workbook(file_staff_1c)
 wb_1c_s = wb_1c.active
-print(wb_1c, wb_1c_s)
-wb_1c_s1 = wb_1c_s.min_column
-wb_1c_s2 = wb_1c_s.min_row
-wb_1c_s3 = wb_1c_s.max_column
-wb_1c_s4 = wb_1c_s.max_row
-print(wb_1c_s1, wb_1c_s2, wb_1c_s3, wb_1c_s4)
+# print(wb_1c, wb_1c_s)
 
-wb_aduser = openpyxl.Workbook()
+wb_1c_s_row_begin = wb_1c_s.min_row
+wb_1c_s_row_end = wb_1c_s.max_row
+wb_1c_s_col_begin = wb_1c_s.min_column
+wb_1c_s_col_end = wb_1c_s.max_column
+
+for number in wb_1c_s.iter_cols(min_col=wb_1c_s_col_begin, max_col=wb_1c_s_col_end,
+                                min_row=wb_1c_s_row_begin, max_row=wb_1c_s_row_end,
+                                values_only=True):
+    print(number)
+
+wb_aduser = openpyxl.load_workbook(file_staff_aduser)
 wb_aduser_s = wb_aduser.active
 print(wb_aduser, wb_aduser_s)
