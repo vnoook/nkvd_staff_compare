ca663cddb02574e71ef3f2e880735f972e8f6d80
noook <noook@yandex.ru>
2026-06-09 15:42:59 +0700

    #5 read files

--- nkvd_staff_compare.py
+++ nkvd_staff_compare.py
@@ -3,6 +3,10 @@
 
 import openpyxl
 
+def str_contains_digit(sequence):
+    return any(character.isdigit() for character in sequence)
+
+
 # файлы данных
 file_staff_1c = 'staff_1c.xlsx'
 file_staff_aduser = 'staff_aduser.xlsx'
@@ -13,17 +17,21 @@ wb_1c_s = wb_1c.active
 # print(wb_1c, wb_1c_s)
 
 # ищу минимальные и максимальные места, где есть данные
-wb_1c_s_row_begin = wb_1c_s.min_row
+wb_1c_s_row_begin = 1  # wb_1c_s.min_row
 wb_1c_s_row_end = wb_1c_s.max_row
-wb_1c_s_col_begin = wb_1c_s.min_column
+wb_1c_s_col_begin = 1  # wb_1c_s.min_column
 wb_1c_s_col_end = wb_1c_s.max_column
 
 # читаю данные из файла
-for number in wb_1c_s.iter_cols(min_col=wb_1c_s_col_begin, max_col=wb_1c_s_col_end,
-                                min_row=wb_1c_s_row_begin, max_row=wb_1c_s_row_end,
-                                values_only=True):
-    print(number)
+for fio_list in wb_1c_s.iter_cols(min_col=wb_1c_s_col_begin, max_col=wb_1c_s_col_end,
+                                  min_row=wb_1c_s_row_begin, max_row=wb_1c_s_row_end,
+                                  values_only=True):
+    pass
+
+for fio in fio_list:
+    if not str_contains_digit(str(fio)):
+        print(fio)
 
 wb_aduser = openpyxl.load_workbook(file_staff_aduser)
 wb_aduser_s = wb_aduser.active
-print(wb_aduser, wb_aduser_s)
+# print(wb_aduser, wb_aduser_s)
