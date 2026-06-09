cf89a7d26934ed92dfdcc610fbeacc287c024f8c
noook <noook@yandex.ru>
2026-06-09 12:07:34 +0700

    #4 read files

--- nkvd_staff_compare.py
+++ nkvd_staff_compare.py
@@ -12,11 +12,13 @@ wb_1c = openpyxl.load_workbook(file_staff_1c)
 wb_1c_s = wb_1c.active
 # print(wb_1c, wb_1c_s)
 
+# ищу минимальные и максимальные места, где есть данные
 wb_1c_s_row_begin = wb_1c_s.min_row
 wb_1c_s_row_end = wb_1c_s.max_row
 wb_1c_s_col_begin = wb_1c_s.min_column
 wb_1c_s_col_end = wb_1c_s.max_column
 
+# читаю данные из файла
 for number in wb_1c_s.iter_cols(min_col=wb_1c_s_col_begin, max_col=wb_1c_s_col_end,
                                 min_row=wb_1c_s_row_begin, max_row=wb_1c_s_row_end,
                                 values_only=True):
